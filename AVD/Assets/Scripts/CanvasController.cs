c26b30c137301160c9a6d57c00549d63fda2c7e2
anleus <tomas.rui598@gmail.com>
2020-02-21 09:12:54 +0100

    ole los caracoles

---
+++ AVD/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasController : MonoBehaviour
+{
+    public Animator titleAnimator;
+
+    public void LoadSelectedScene(int i)
+    {
+        SceneManager.LoadSceneAsync(i);
+    }
+
+    public void TitleAction()
+    {
+        titleAnimator.SetTrigger("touched");
+    }
+}
