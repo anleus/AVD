334bf58b8b8ec8c1bcdf04e8ecbeb3345f18c851
Tomás Ruiz <tomas.rui598@gmail.com>
2020-02-28 19:25:41 +0100

    disparos

---
+++ AVD/Assets/Scripts/Bullet3D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet3D : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 0, speed, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AVD/Assets/Turret.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform point1;
+    public Transform point2;
+    public Transform point3;
+    public Transform point4;
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("Fire1")) Shot();
+    }
+
+    public void Shot()
+    {
+        Instantiate(bullet, point1.position, point1.rotation);
+        
+        Instantiate(bullet, point2.position, point2.rotation);
+     
+        Instantiate(bullet, point3.position, point3.rotation);
+  
+        Instantiate(bullet, point4.position, point4.rotation);
+    }
+}
