7697c1ba6029c00cf37315e553e5a43199cfec9b
Tomás Ruiz <tomas.rui598@gmail.com>
2020-03-06 19:33:57 +0100

    hola torreta

---
+++ AVD/Assets/Scripts/BallShooting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooting : MonoBehaviour
+{
+    public GameObject ball;
+    private GameObject createdBall;
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            Vector3 direction = mousePosition - transform.position;
+
+            //RaycastHit hitPosition = Physics.Raycast(transform.position, direction);
+            
+            ShotBall(mousePosition);
+        }
+    }
+
+    void ShotBall(Vector3 position)
+    {
+        createdBall = Instantiate(ball, transform.position, transform.rotation);
+        createdBall.GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
+}
---
+++ AVD/Assets/Scripts/TurretBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LayerMask layerMask;
+    public GameObject turret;
+
+    private Rigidbody rb;
+    private Transform hit;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddTorque(transform.forward);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (layerMask == (layerMask | (1 << collision.gameObject.layer)))
+        {
+            Instantiate(turret, collision.transform.position + new Vector3(0f, 0.5f, 0f), collision.transform.rotation);
+        }
+
+        Destroy(gameObject);
+    }
+}
