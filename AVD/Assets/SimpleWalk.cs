995f1accdd0db48c98b6f2bbd038d7dd8610779f
anleus <tomas.rui598@gmail.com>
2020-03-20 19:56:34 +0100

    otro dia mas en la oficina

---
+++ AVD/Assets/SimpleWalk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleWalk : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public Transform destination;
+    Vector3 previousPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = destination.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destination.transform.position != previousPosition)
+        {
+            agent.destination = destination.transform.position;
+            previousPosition = destination.transform.position;
+        }
+    }
+}
