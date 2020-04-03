21242c249bb496b24f602750ef2906f7564607c0
anleus <tomas.rui598@gmail.com>
2020-04-03 19:31:13 +0200

    clase 3 abril

---
+++ AVD/Assets/ClassThings/ChestControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class ChestControl : MonoBehaviour
+{
+    void Play()
+    {
+        GetComponent<PlayableDirector>().Play();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Play();
+        }
+    }
+}
